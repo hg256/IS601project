1f14d42dc1d9f3643244c295fd6af4d84e133220
himakeerthi <hg256@njit.edu>
2018-10-06 20:19:48 -0400

    Task : created main class and method to fetch getrecords from CSV.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: himakeerthi
- * Date: 10/4/18
- * Time: 2:58 PM
+main::start("example.csv");
+/* Description: Class to initialize main
+ * Parameters: filename, variable used to read the CSV file content.
+ * Result: read records from CSV file and display in html table.
  */
-echo 'Test heroku and github';
+class main  {
+    static public function start($filename) {
+        $records = csv::getRecords($filename);
+    }
+}
+
+/* Description: Class to initialize records as key - value pair from CSV file
+ * Parameters: $filename, variable used to obtain the CSV file contents.
+ * Result: return an array with CSV contents as key-value pair.
+ */
+
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                //Create records Array
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
 ?>
